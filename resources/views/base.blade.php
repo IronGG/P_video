d6c37ba3aeba2413e5494cf5efa46f876dd60656
IronGG <clsadri@gmail.com>
2021-03-29 16:12:12 +0200

    Profile section included and working

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -78,15 +78,58 @@
                   <div class="border-l-2 border-black"></div>
                   <a href="{{ route('register') }}" class="px-4 py-2 font-bold bg-red-400 rounded-tr-md rounded-br-md focus:bg-red-500 focus:ring-2 focus:ring-red-300">Inscription</a>
                 @else
-                <form method="POST" action="{{ route('logout') }}">
-                  @csrf
+                <!-- Settings Dropdown -->
+                <div class="relative ml-3">
+                  <x-jet-dropdown align="right" width="48">
+                      <x-slot name="trigger">
+                          @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                              <button class="flex text-sm transition duration-150 ease-in-out border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300">
+                                  <img class="object-cover w-12 h-12 rounded-full  ring-4 ring-green-400" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                              </button>
+                          @else
+                              <span class="inline-flex rounded-md">
+                                  <button type="button" class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md hover:text-gray-700 focus:outline-none">
+                                      {{ Auth::user()->name }}
 
-                  <x-jet-dropdown-link href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                                  this.closest('form').submit();">
-                      {{ __('Logout') }}
-                  </x-jet-dropdown-link>
-                </form>
+                                      <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                          <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                      </svg>
+                                  </button>
+                              </span>
+                          @endif
+                      </x-slot>
+
+                      <x-slot name="content">
+                          <!-- Account Management -->
+                          <div class="block px-4 py-2 text-xs text-gray-400">
+                              {{ __('Manage Account') }}
+                          </div>
+
+                          <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                              {{ __('Profile') }}
+                          </x-jet-dropdown-link>
+
+                          @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                              <x-jet-dropdown-link href="{{ route('api-tokens.index') }}">
+                                  {{ __('API Tokens') }}
+                              </x-jet-dropdown-link>
+                          @endif
+
+                          <div class="border-t border-gray-100"></div>
+
+                          <!-- Authentication -->
+                          <form method="POST" action="{{ route('logout') }}">
+                              @csrf
+
+                              <x-jet-dropdown-link href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                              this.closest('form').submit();">
+                                  {{ __('Log Out') }}
+                              </x-jet-dropdown-link>
+                          </form>
+                      </x-slot>
+                  </x-jet-dropdown>
+              </div>
                 @endif
               </div>
             </div>
